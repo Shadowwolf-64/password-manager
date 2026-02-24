399b06c89dd7112ca1fa5800b7e661e1fd2fa1a8
ebonwulf <ebonwolf00@gmail.com>
2026-02-24 14:13:26 +0000

    Added the logo image to the panel in accountsPanel.py

--- accountsPanel.py
+++ accountsPanel.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from PIL import Image
 
 def accountsPanel():
     ctk.set_appearance_mode("system") # "light" or "system" or "dark"
@@ -7,6 +8,14 @@ def accountsPanel():
     app = ctk.CTk() 
     app.title("Forget me not password manager") 
     app.geometry("700x420") 
+
+    # Images #
+    logoImage = ctk.CTkImage(light_image=Image.open('images/logoNoBg.png'), 
+                             dark_image=Image.open('images/logoNoBg.png'),
+                             size=(200, 100))
+
+    logoLabel = ctk.CTkLabel(app, text="", image=logoImage)
+    logoLabel.pack(pady=1)
 
     app.mainloop()
 
